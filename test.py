85f11034bc6e321296509bfe4d3e1c81470d87ba
Miodrag Nedeljkovic <kyryloheiko3@gmail.com>
2025-02-04 21:04:17 -0600

    Update test.py

--- test.py
+++ test.py
@@ -66,8 +66,8 @@ async def main():
         await page.wait_for_selector("text='Research & Learn'")
         await page.click("text='Research & Learn'")
 
-        await page.wait_for_selector("#content-container")
-        await page.click("#content-container .col-xs-12 > .row > .col-xs-4:nth-of-type(3) a img")
+        await page.wait_for_selector("#main-content")
+        await page.click("#main-content .col-xs-12 > .row > .col-xs-4:nth-of-type(3) a img")
 
         await page.wait_for_selector("text='Open Resource'")
         await page.click("text='Open Resource'")
